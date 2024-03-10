e9c1c851165964edc486655c272ed88eb75159cd
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-03-10 20:34:56 -0300

    Update e3.sh

--- e3.sh
+++ e3.sh
@@ -19,3 +19,7 @@ yay -S --noconfirm nano --save --answerdiff None --answerclean None --removemake
 sudo rm -r /etc/pacman.conf && sudo mv ~/AutoArch/pacman.conf /etc;
 
 sudo rm -r ~/.bashrc && sudo mv ~/AutoArch/.bashrc ~;
+
+sudo rm -r /etc/pacman.d/mirrorlist && sudo mv ~/AutoArch/mirrorlist /etc/pacman.d;
+
+sudo rm -r /etc/pacman.d/mirrorlist-arch && sudo mv ~/AutoArch/mirrorlist-arch /etc/pacman.d;
