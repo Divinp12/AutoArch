90d1b43b900c45b27300cd7918f1ddd66f66e09d
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-03-09 21:28:52 -0300

    Atualizar o e2.sh

--- e2.sh
+++ e2.sh
@@ -1,1 +1,31 @@
 #!/bin/bash
+
+arch-chroot /mnt <<END
+
+echo arch >> /etc/hostname;
+
+passwd root;
+
+useradd -m -g users -G wheel arch;
+
+passwd arch;
+
+mkinitcpio -P;
+
+pacman -S networkmanager grub efibootmgr xf86-video-intel sudo mesa fastfetch lxdm nano xfwm4 xfce4-panel xfdesktop thunar xfce4-session xfce4-settings xfce4-terminal xfconf pipewire pipewire-pulse pipewire-media-session pavucontrol --noconfirm;
+
+systemctl enable lxdm;
+
+systemctl enable NetworkManager;
+
+systemctl disable NetworkManager-wait-online.service;
+
+systemctl disable systemd-timesyncd.service;
+
+grub-install --target=x86_64-efi --bootloader-id=arch --recheck;
+
+grub-mkconfig -o /boot/grub/grub.cfg;
+
+END
+
+reboot
