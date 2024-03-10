75abada622c1788096015298dfad848cc42f6e68
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-03-10 19:40:59 -0300

    Update e3.sh

--- e3.sh
+++ e3.sh
@@ -1,1 +1,15 @@
 !#/bin/bash
+
+git clone https://aur.archlinux.org/yay.git;
+
+sudo chmod 777 yay;
+
+cd yay;
+
+makepkg -si --noconfirm;
+
+cd ~;
+
+sudo rm -r yay;
+
+yay -S --noconfirm nano --save --answerdiff None --answerclean None --removemake;
