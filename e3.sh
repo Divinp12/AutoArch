84b3b0166e09f6128b7f4bdbb840fd93e6c67015
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-03-11 09:05:00 -0300

    Update e3.sh

--- e3.sh
+++ e3.sh
@@ -14,7 +14,7 @@ cd ~;
 
 sudo rm -r yay;
 
-yay -S --noconfirm google-chrome --save --answerdiff None --answerclean None --removemake;
+yay -S --noconfirm google-chrome mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader vulkan-validation-layers lib32-vulkan-validation-layers vulkan-tools lib32-vulkan-tools vulkan-mesa-layers lib32-vulkan-mesa-layers vulkan-headers mesa-vdpau lib32-mesa-vdpau libva-mesa-driver lib32-libva-mesa-driver --save --answerdiff None --answerclean None --removemake;
 
 sudo rm -r /etc/pacman.conf && sudo mv ~/AutoArch/pacman.conf /etc;
 
