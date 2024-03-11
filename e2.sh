86f428c9ecf0dacac8e19013cf8d16d6ee9d4984
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-03-11 11:40:22 -0300

    Update e2.sh

--- e2.sh
+++ e2.sh
@@ -18,11 +18,12 @@ systemctl enable NetworkManager;
 systemctl disable NetworkManager-wait-online.service;
 systemctl disable systemd-timesyncd.service;
 
+ln -sf /usr/share/zoneinfo/America/Sao_paulo /etc/localtime;
 echo "pt_BR.UTF-8 UTF-8" >> /etc/locale.gen;
 echo "LANG=pt_BR.UTF-8" >> /etc/locale.conf;
-echo "arch ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers;
+hwclock --systohc && locale-gen;
 
-locale-gen;
+echo "arch ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers;
 
 grub-install --target=x86_64-efi --bootloader-id=arch --recheck;
 grub-mkconfig -o /boot/grub/grub.cfg
