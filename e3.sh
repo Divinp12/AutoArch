8eaa37964d9b21c41f50e358b171a69bbcf08f9f
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-03-11 20:59:31 -0300

    Update e3.sh

--- e3.sh
+++ e3.sh
@@ -2,8 +2,8 @@
 
 sudo rm -r /etc/pacman.conf && sudo mv ~/AutoArch/pacman.conf /etc;
 sudo rm -r ~/.bashrc && sudo mv ~/AutoArch/.bashrc ~;
-sudo rm -r /etc/pacman.d/mirrorlist && su root echo "Server=https://mirror.ufscar.br/archlinux/$repo/os/$arch" >> /etc/pacman.d/mirrorlist;
-sudo rm -r /etc/pacman.d/mirrorlist-arch && su root echo "Server=https://mirror.ufscar.br/archlinux/$repo/os/$arch" >> /etc/pacman.d/mirrorlist-arch;
+sudo rm -r /etc/pacman.d/mirrorlist && sudo su root echo "Server=https://mirror.ufscar.br/archlinux/$repo/os/$arch" >> /etc/pacman.d/mirrorlist;
+sudo rm -r /etc/pacman.d/mirrorlist-arch && sudo su root echo "Server=https://mirror.ufscar.br/archlinux/$repo/os/$arch" >> /etc/pacman.d/mirrorlist-arch;
 
 sudo pacman -Syyu --noconfirm;
 
