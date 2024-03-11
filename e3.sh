ddc85ff7eb3c24d1b193707c15b8c4c51070c961
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-03-11 12:05:26 -0300

    Update e3.sh

--- e3.sh
+++ e3.sh
@@ -17,11 +17,8 @@ sudo rm -r yay;
 yay -S --noconfirm google-chrome pipewire pipewire-pulse pipewire-media-session pavucontrol mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader vulkan-validation-layers lib32-vulkan-validation-layers vulkan-tools lib32-vulkan-tools vulkan-mesa-layers lib32-vulkan-mesa-layers vulkan-headers mesa-vdpau lib32-mesa-vdpau libva-mesa-driver lib32-libva-mesa-driver --save --answerdiff None --answerclean None --removemake;
 
 sudo rm -r /etc/pacman.conf && sudo mv ~/AutoArch/pacman.conf /etc;
-
 sudo rm -r ~/.bashrc && sudo mv ~/AutoArch/.bashrc ~;
-
 sudo rm -r /etc/pacman.d/mirrorlist && sudo mv ~/AutoArch/mirrorlist /etc/pacman.d;
-
 sudo rm -r /etc/pacman.d/mirrorlist-arch && sudo mv ~/AutoArch/mirrorlist-arch /etc/pacman.d;
 
 sudo pacman -Syyu --noconfirm;
