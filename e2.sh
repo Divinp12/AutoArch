1989dc8d57a8fdd1290ce54678422d3c928b23d3
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-03-12 08:04:51 -0300

    Update e2.sh

--- e2.sh
+++ e2.sh
@@ -25,8 +25,25 @@ hwclock --systohc && locale-gen;
 
 echo "arch ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers;
 
-rm -r /etc/pacman.d/mirrorlist && echo "Server=https://mirror.ufscar.br/archlinux/$repo/os/$arch" >> /etc/pacman.d/mirrorlist;
-rm -r /etc/pacman.d/mirrorlist-arch && echo "Server=https://mirror.ufscar.br/archlinux/$repo/os/$arch" >> /etc/pacman.d/mirrorlist-arch;
+rm -r /etc/pacman.d/mirrorlist;
+echo "Server=https://mirror.ufscar.br/archlinux/$repo/os/$arch" >> /etc/pacman.d/mirrorlist;
+
+rm -r /etc/pacman.d/mirrorlist-arch;
+echo "Server=https://mirror.ufscar.br/archlinux/$repo/os/$arch" >> /etc/pacman.d/mirrorlist-arch;
+
+rm -r /home/arch/.bashrc
+echo "alias i='yay -S --noconfirm'" >> /home/arch/.bashrc;
+echo "alias d='sudo pacman -Rsc'" >> /home/arch/.bashrc;
+echo "alias nano='sudo nano'" >> /home/arch/.bashrc;
+echo "alias addsuporte-bluetooth='yay -S --noconfirm bluez bluez-tools bluez-utils blueman; sudo systemctl start bluetooth.service; sudo systemctl enable bluetooth.service'" >> /home/arch/.bashrc;
+echo "sudo pacman-key --refresh-keys;" >> /home/arch/.bashrc;
+echo "sudo rm -r ~/.bash_history;" >> /home/arch/.bashrc;
+echo "sudo rm -r ~/.cache;" >> /home/arch/.bashrc;
+echo "sudo pacman -Syyu --noconfirm;" >> /home/arch/.bashrc;
+echo "sudo pacman -Scc --noconfirm;" >> /home/arch/.bashrc;
+echo "sudo history -c;" >> /home/arch/.bashrc;
+echo "clear;" >> /home/arch/.bashrc;
+echo "fastfetch" >> /home/arch/.bashrc;
 
 pacman -Syyu --noconfirm;
 
