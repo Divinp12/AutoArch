5c084507f70c267d47bb8fdd57553cceaef05216
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-03-12 07:16:16 -0300

    Update e3.sh

--- e3.sh
+++ e3.sh
@@ -7,12 +7,7 @@ sudo rm -r /etc/pacman.d/mirrorlist-arch && sudo su root echo "Server=https://mi
 
 sudo su arch pacman -Syyu --noconfirm;
 
-git clone https://aur.archlinux.org/yay.git;
-sudo chmod 777 yay;
-cd yay;
-makepkg -si --noconfirm;
-cd ~;
-sudo rm -r yay;
+git clone https://aur.archlinux.org/yay.git; sudo chmod 777 yay; cd yay; makepkg -si --noconfirm; cd ~; sudo rm -r yay;
 yay -S --noconfirm google-chrome pipewire pipewire-pulse pipewire-media-session pavucontrol mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader vulkan-validation-layers lib32-vulkan-validation-layers vulkan-tools lib32-vulkan-tools vulkan-mesa-layers lib32-vulkan-mesa-layers vulkan-headers mesa-vdpau lib32-mesa-vdpau libva-mesa-driver lib32-libva-mesa-driver --save --answerdiff None --answerclean None --removemake;
 
 sudo rm -r ~/AutoArch && reboot
