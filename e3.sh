eca042dcf5e6b1dde702068afe7afee0e4eb1738
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-03-18 09:36:30 -0300

    Delete e3.sh

--- e3.sh
+++
@@ -1,4 +0,0 @@
-git clone https://aur.archlinux.org/yay.git; sudo chmod 777 yay; cd yay; makepkg -si --noconfirm; cd ~; sudo rm -r yay;
-yay -S --noconfirm google-chrome pipewire pipewire-pulse pipewire-media-session pavucontrol mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader vulkan-validation-layers lib32-vulkan-validation-layers vulkan-tools lib32-vulkan-tools vulkan-mesa-layers lib32-vulkan-mesa-layers vulkan-headers mesa-vdpau lib32-mesa-vdpau libva-mesa-driver lib32-libva-mesa-driver --save --answerdiff None --answerclean None --removemake;
-
-sudo rm -r ~/AutoArch && reboot
