8d9dfd1ca03e93ee353295e08aeca4503b480d62
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-03-18 08:51:37 -0300

    Update e1.sh

--- e1.sh
+++ e1.sh
@@ -3,14 +3,10 @@
 dd if=/dev/zero of=/dev/sda status=progress
 
 parted /dev/sda mklabel gpt \
-mkpart "BIOS Boot" 1MB 1GB \
+mkpart "BIOS Boot" fat32 1MB 1GB \
 set 1 bios_grub on \
-mkpart "Root" 1GB 21GB \
-mkpart "Home" 21GB 100%;
-
-mkfs.fat -F32 /dev/sda1;
-mkfs.ext4 /dev/sda2;
-mkfs.ext4 /dev/sda3;
+mkpart "Root" ext4 1GB 21GB \
+mkpart "Home" ext4 21GB 100%;
 
 mount /dev/sda2 /mnt;
 
