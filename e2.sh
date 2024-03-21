f959938ccc36f35649005ca10e7854027371cf21
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-03-21 11:42:43 -0300

    Update e2.sh

--- e2.sh
+++ e2.sh
@@ -28,8 +28,8 @@ echo arch > /etc/hostname && yes arch | passwd root
 && echo "alias nano='sudo nano'" >> /home/arch/.bashrc
 && echo "alias addsuporte-bluetooth='yay -S --noconfirm bluez bluez-tools bluez-utils blueman; sudo systemctl start bluetooth.service; sudo systemctl enable bluetooth.service'" >> /home/arch/.bashrc
 && echo "sudo pacman-key --refresh-keys;" >> /home/arch/.bashrc
-&& echo "sudo rm -r ~/.bash_history;" >> /home/arch/.bashrc
-&& echo "sudo rm -r ~/.cache;" >> /home/arch/.bashrc
+&& echo "sudo rm -rf ~/.bash_history;" >> /home/arch/.bashrc
+&& echo "sudo rm -rf ~/.cache;" >> /home/arch/.bashrc
 && echo "sudo pacman -Syyu --noconfirm;" >> /home/arch/.bashrc
 && echo "sudo pacman -Scc --noconfirm;" >> /home/arch/.bashrc
 && echo "sudo history -c;" >> /home/arch/.bashrc
