78d7e7bd931504d4ffdc4d489bf7a739b252f8a9
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-03-20 21:09:51 -0300

    Update e2.sh

--- e2.sh
+++ e2.sh
@@ -79,7 +79,7 @@ echo "autologin=arch" >> /etc/lxdm/lxdm.conf;
 
 pacman -Syyu --noconfirm;
 
-git clone https://aur.archlinux.org/yay.git; chmod 777 yay; cd yay; makepkg -si --noconfirm; cd ..; rm -r yay;
+git clone https://aur.archlinux.org/yay.git; chmod 777 yay; cd yay; makepkg -si --noconfirm; cd ..; rm -rf yay;
 yay -S --noconfirm google-chrome pipewire pipewire-pulse pipewire-media-session pavucontrol mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader vulkan-validation-layers lib32-vulkan-validation-layers vulkan-tools lib32-vulkan-tools vulkan-mesa-layers lib32-vulkan-mesa-layers vulkan-headers mesa-vdpau lib32-mesa-vdpau libva-mesa-driver lib32-libva-mesa-driver --save --answerdiff None --answerclean None --removemake;
 yay -S --noconfirm google-chrome pipewire pipewire-pulse pipewire-media-session pavucontrol mesa lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader vulkan-validation-layers lib32-vulkan-validation-layers vulkan-tools lib32-vulkan-tools vulkan-mesa-layers lib32-vulkan-mesa-layers vulkan-headers mesa-vdpau lib32-mesa-vdpau libva-mesa-driver lib32-libva-mesa-driver;
 
