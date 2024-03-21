17e2a829b848ff6bbc74df28b117c4fcf459b37e
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-03-21 11:59:33 -0300

    Update e2.sh

--- e2.sh
+++ e2.sh
@@ -1,28 +1,6 @@
 #!/bin/bash
 
-echo arch > /etc/hostname && yes arch | passwd root
-
-&& useradd -m -g users -G wheel arch && yes arch | passwd arch
-
-&& mkinitcpio -P
-
-&& pacman -S networkmanager git grub efibootmgr sudo mesa fastfetch lightdm xfwm4 xfce4-panel xfdesktop thunar xfce4-session xfce4-settings xfce4-terminal xfconf pipewire pipewire-pulse pipewire-media-session pavucontrol intel-ucode amd-ucode --noconfirm
-
-&& systemctl enable lightdm && systemctl enable NetworkManager
-
-&& systemctl disable NetworkManager-wait-online && systemctl disable systemd-timesyncd
-
-&& ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime && echo "pt_BR.UTF-8 UTF-8" > /etc/locale.gen && echo "LANG=pt_BR.UTF-8" > /etc/locale.conf && hwclock --systohc && locale-gen
-
-&& sed -i '/^\[Seat:\*]/a autologin-user=arch' /etc/lightdm/lightdm.conf && groupadd -r autologin && gpasswd -a arch autologin
-
-&& echo "arch ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
-
-&& rm -rf /etc/pacman.d/mirrorlist && rm -rf /etc/pacman.d/mirrorlist && echo 'Server=https://mirror.ufscar.br/archlinux/'$'$repo/os/'$'$arch' > /etc/pacman.d/mirrorlist
-
-&& rm -rf /etc/pacman.d/mirrorlist-arch && rm -rf /etc/pacman.d/mirrorlist-arch && echo 'Server=https://mirror.ufscar.br/archlinux/'$'$repo/os/'$'$arch' > /etc/pacman.d/mirrorlist-arch
-
-&& rm -rf /home/arch/.bashrc && rm -rf /home/arch/.bashrc
+echo arch > /etc/hostname && yes arch | passwd root && useradd -m -g users -G wheel arch && yes arch | passwd arch && mkinitcpio -P && pacman -S networkmanager git grub efibootmgr sudo mesa fastfetch lightdm xfwm4 xfce4-panel xfdesktop thunar xfce4-session xfce4-settings xfce4-terminal xfconf pipewire pipewire-pulse pipewire-media-session pavucontrol intel-ucode amd-ucode --noconfirm && systemctl enable lightdm && systemctl enable NetworkManager && systemctl disable NetworkManager-wait-online && systemctl disable systemd-timesyncd && ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime && echo "pt_BR.UTF-8 UTF-8" > /etc/locale.gen && echo "LANG=pt_BR.UTF-8" > /etc/locale.conf && hwclock --systohc && locale-gen && sed -i '/^\[Seat:\*]/a autologin-user=arch' /etc/lightdm/lightdm.conf && groupadd -r autologin && gpasswd -a arch autologin && echo "arch ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers && rm -rf /etc/pacman.d/mirrorlist && rm -rf /etc/pacman.d/mirrorlist && echo 'Server=https://mirror.ufscar.br/archlinux/'$'$repo/os/'$'$arch' > /etc/pacman.d/mirrorlist && rm -rf /etc/pacman.d/mirrorlist-arch && rm -rf /etc/pacman.d/mirrorlist-arch && echo 'Server=https://mirror.ufscar.br/archlinux/'$'$repo/os/'$'$arch' > /etc/pacman.d/mirrorlist-arch && rm -rf /home/arch/.bashrc && rm -rf /home/arch/.bashrc
 && echo "alias i='yay -Ss --noconfirm'" > /home/arch/.bashrc
 && echo "alias d='sudo pacman -Rsc'" >> /home/arch/.bashrc
 && echo "alias nano='sudo nano'" >> /home/arch/.bashrc
