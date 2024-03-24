be0e4894c70cf46cca2123d9adb4950bca2192a9
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-03-23 23:17:58 -0300

    Update e2.sh

--- e2.sh
+++ e2.sh
@@ -76,5 +76,5 @@ systemctl disable NetworkManager-wait-online;
 pacman -S pipewire pipewire-pulse pipewire-media-session pavucontrol --noconfirm;
 
 pacman -S grub-efi-x86_64 efibootmgr;
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=z --recheck;
+grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=z --recheck;
 grub-mkconfig -o /boot/grub/grub.cfg;
