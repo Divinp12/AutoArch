e15f7cd25acfea9d47220e8739c75eacaedb094b
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-03-24 14:13:09 -0300

    Update e1.sh

--- e1.sh
+++ e1.sh
@@ -6,16 +6,16 @@ mkfs.fat -F32 /dev/sda1 > /dev/null;
 mkfs.ext4 /dev/sda2 > /dev/null;
 mkfs.ext4 /dev/sda3 > /dev/null;
 
-mount /dev/sda2 /mnt;
+mount /dev/sda2 /mnt > /dev/null;
 
-mkdir /mnt/boot;
-mkdir /mnt/boot/EFI;
-mkdir /mnt/home;
+mkdir /mnt/boot > /dev/null;
+mkdir /mnt/boot/EFI > /dev/null;
+mkdir /mnt/home > /dev/null;
 
-mount /dev/sda1 /mnt/boot/EFI;
-mount /dev/sda3 /mnt/home;
+mount /dev/sda1 /mnt/boot/EFI > /dev/null;
+mount /dev/sda3 /mnt/home > /dev/null;
 
-pacstrap /mnt base base-devel linux linux-firmware;
+pacstrap /mnt base base-devel linux linux-firmware > /dev/null;
 
 genfstab -U -p /mnt >> /mnt/etc/fstab;
 
