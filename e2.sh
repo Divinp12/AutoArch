d624a9f24257731341c2f55e07b5f4d4c8f2213d
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-03-26 07:34:23 -0300

    Update e2.sh

--- e2.sh
+++ e2.sh
@@ -39,7 +39,7 @@ echo "sudo pacman -Scc --noconfirm > /dev/null;" >> ~/.bashrc;
 echo "sudo history -c > /dev/null;" >> ~/.bashrc;
 echo "clear;" >> ~/.bashrc;
 echo "fastfetch" >> ~/.bashrc;
-echo "git clone https://aur.archlinux.org/yay.git; sudo chmod 777 yay; cd yay; makepkg -si --noconfirm; cd ..; sudo rm -rf yay; yay -S --noconfirm google-chrome lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader vulkan-validation-layers lib32-vulkan-validation-layers vulkan-tools lib32-vulkan-tools vulkan-mesa-layers lib32-vulkan-mesa-layers vulkan-headers mesa-vdpau lib32-mesa-vdpau libva-mesa-driver lib32-libva-mesa-driver --save --answerdiff None --answerclean None --removemake; sed -i '$ d' /home/arch/.bashrc" >> /home/arch/.bashrc;
+echo "git clone https://aur.archlinux.org/yay.git; sudo chmod 777 yay; cd yay; makepkg -si --noconfirm; cd ..; sudo rm -rf yay; yay -S --noconfirm google-chrome lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader vulkan-validation-layers lib32-vulkan-validation-layers vulkan-tools lib32-vulkan-tools vulkan-mesa-layers lib32-vulkan-mesa-layers vulkan-headers mesa-vdpau lib32-mesa-vdpau libva-mesa-driver lib32-libva-mesa-driver --save --answerdiff None --answerclean None --removemake; sed -i '$ d' ~/.bashrc" >> ~/.bashrc;
 
 rm -rf /etc/pacmam.conf > /dev/null;
 rm -rf /etc/pacman.conf > /dev/null;
