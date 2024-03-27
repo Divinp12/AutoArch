81616114728d6efe943878f7fdf9fbf8bcd524db
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-03-27 09:25:05 -0300

    Update e2.sh

--- e2.sh
+++ e2.sh
@@ -18,13 +18,19 @@ echo "LANG=pt_BR.UTF-8" > /etc/locale.conf;
 hwclock --systohc > /dev/null;
 locale-gen > /dev/null;
 
-rm -rf /etc/pacman.d/mirrorlist{,} > /dev/null;
+rm -rf /etc/pacman.d/mirrorlist > /dev/null;
+rm -rf /etc/pacman.d/mirrorlist > /dev/null;
+rm -rf /etc/pacman.d/mirrorlist > /dev/null;
 echo 'Server=https://mirror.ufscar.br/archlinux/'$'$repo/os/'$'$arch' > /etc/pacman.d/mirrorlist;
 
-rm -rf /etc/pacman.d/mirrorlist-arch{,} > /dev/null;
+rm -rf /etc/pacman.d/mirrorlist-arch > /dev/null;
+rm -rf /etc/pacman.d/mirrorlist-arch > /dev/null;
+rm -rf /etc/pacman.d/mirrorlist-arch > /dev/null;
 echo 'Server=https://mirror.ufscar.br/archlinux/'$'$repo/os/'$'$arch' > /etc/pacman.d/mirrorlist-arch;
 
-rm -rf ~/.bashrc{,} > /dev/null;
+rm -rf ~/.bashrc > /dev/null;
+rm -rf ~/.bashrc > /dev/null;
+rm -rf ~/.bashrc > /dev/null;
 echo "alias i='yay -S --noconfirm'" > ~/.bashrc;
 echo "alias d='sudo pacman -Rsc'" >> ~/.bashrc;
 echo "alias nano='sudo nano'" >> ~/.bashrc;
@@ -38,7 +44,9 @@ echo "clear;" >> ~/.bashrc;
 echo "fastfetch" >> ~/.bashrc;
 echo "git clone https://aur.archlinux.org/yay.git; sudo chmod 777 yay; cd yay; makepkg -si --noconfirm; cd ..; sudo rm -rf yay; yay -S --noconfirm google-chrome lib32-mesa vulkan-radeon lib32-vulkan-radeon vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader vulkan-validation-layers lib32-vulkan-validation-layers vulkan-tools lib32-vulkan-tools vulkan-mesa-layers lib32-vulkan-mesa-layers vulkan-headers mesa-vdpau lib32-mesa-vdpau libva-mesa-driver lib32-libva-mesa-driver --save --answerdiff None --answerclean None --removemake; sed -i '$ d' ~/.bashrc" >> ~/.bashrc;
 
-rm -rf /etc/pacmam.conf{,} > /dev/null;
+rm -rf /etc/pacmam.conf > /dev/null;
+rm -rf /etc/pacman.conf > /dev/null;
+rm -rf /etc/pacman.conf > /dev/null;
 echo "[options]" > /etc/pacman.conf;
 echo "HoldPkg=pacman glibc" >> /etc/pacman.conf;
 echo "Architecture=auto" >> /etc/pacman.conf;
