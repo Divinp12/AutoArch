4cf42b73915f85237a007dbd2cf8400be20d331a
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-03-27 19:35:12 -0300

    Update e1.sh

--- e1.sh
+++ e1.sh
@@ -1,19 +1,6 @@
 #!/bin/bash
 
-TOTAL_SECTORS=$(blockdev --getsz "/dev/sda")
-
-BIOS_BOOT_SIZE=$((1024*1024*2))
-ROOT_SIZE=$((1024*1024*20))
-
-REST_SIZE=$(($TOTAL_SECTORS - $BIOS_BOOT_SIZE - $ROOT_SIZE))
-
-sgdisk --clear \
-       --new=1:2048:+${BIOS_BOOT_SIZE} --typecode=1:ef02 --change-name=1:"BIOS boot" \
-       --new=2:0:+${ROOT_SIZE} --typecode=2:8300 --change-name=2:"Raiz (/)" \
-       --new=3:0:+${REST_SIZE} --typecode=3:8300 --change-name=3:"Outros" \
-       --print /dev/sda
-
-partprobe
+cfdisk /dev/sda;
 
 mkfs.fat -F32 /dev/sda1 > /dev/null;
 mkfs.ext4 -F /dev/sda2 > /dev/null;
