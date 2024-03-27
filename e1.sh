0a0d7985a1f2c512e6fd43768cd02437966729c7
Divinp123 <142677856+Divinp12@users.noreply.github.com>
2024-03-27 08:54:35 -0300

    Update e1.sh

--- e1.sh
+++ e1.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
+# Limpar a tabela de partições existente no dispositivo
+echo "o
+w
+" | fdisk /dev/sda
 
-cfdisk /dev/sda;
+# Criar partição para /boot (1GB)
+echo "n
+p
+1
+
++1G
+" | fdisk /dev/sda
+
+# Criar partição para /
+echo "n
+p
+2
+
++20G
+" | fdisk /dev/sda
+
+# Criar partição para /home (usando o restante do espaço disponível)
+echo "n
+p
+3
+
+
+w
+" | fdisk /dev/sda
 
 mkfs.fat -F32 /dev/sda1 > /dev/null;
 mkfs.ext4 -F /dev/sda2 > /dev/null;
